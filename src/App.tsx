d3306bdbde089986558af02c4663e5aa43b99129
pbutcher <pbutcher93@gmail.com>
2022-04-13 15:44:24 +0100

    MVP demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,55 @@
 //We know this demo from https://codesandbox.io/s/react-xr-hands-demo-gczkp?file=/src/index.tsx works;
-import { OrbitControls } from '@react-three/drei/core/OrbitControls'
-import { Sky } from '@react-three/drei/core/Sky';
-import ReactDOM from 'react-dom';
-import React, { Component, Suspense, useEffect, useReducer, useState } from 'react';
-import { VRCanvas, Hands, DefaultXRControllers } from '@react-three/xr';
-import { Dictaphone } from './Components/Verbal/SpeechToText.js';
-import './App.css';
-import { reducer, initialState, DispatchContext } from './utils/Reducer';
-import { socket, setupSocketEvents } from './utils/Socket';
-import { GesturePrimitives, SpellPages } from 'spellbook';
-import MageHand from './Components/Somatic/MageHand';
-import Interpreter from './SpellCasting/Interpreter';
+import { OrbitControls } from "@react-three/drei/core/OrbitControls";
+import { Sky } from "@react-three/drei/core/Sky";
+import ReactDOM from "react-dom";
+import React, {
+  Component,
+  Suspense,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
+import { VRCanvas, Hands, DefaultXRControllers } from "@react-three/xr";
+import { Dictaphone } from "./Components/Verbal/SpeechToText.js";
+import "./App.css";
+import { reducer, initialState, DispatchContext } from "./utils/Reducer";
+import { socket, setupSocketEvents } from "./utils/Socket";
+import { GesturePrimitives, SpellPages } from "spellbook";
+import MageHand from "./Components/Somatic/MageHand";
+import Interpreter from "./SpellCasting/Interpreter";
 //const spellbook = require('spellbook');
 //import client from './utils/socketConfig';
+import * as THREE from "three";
 
+// ******************** Optomancy ********************
+import Optomancy from "optomancy";
+import iris from "./assets/iris.json";
+declare global {
+  interface Window {
+    OptomancyNS: any;
+  }
+}
+
+window.OptomancyNS = window.OptomancyNS || {};
+const transitionConfig = {
+  title: "The Iris Flower Dataset",
+  data: { values: iris },
+};
+const optomancyConfig = {
+  data: iris,
+};
+// ***************************************************
 
 // Hololens user agent is going to be a version of edge above the latest release
 let ua = navigator.userAgent.toLowerCase();
-console.log(ua)
-let isHL = ua.replace('edg', '').length < ua.length;
-let primitives : any = []
-for(let prim in GesturePrimitives()){
-  primitives.push(prim)
+console.log(ua);
+let isHL = ua.replace("edg", "").length < ua.length;
+let primitives: any = [];
+for (let prim in GesturePrimitives()) {
+  primitives.push(prim);
 }
 //const primitives: any = [];
 //console.log(primitives);
- 
 
 // Not sure why other joint pos demo breaks, but https://codesandbox.io/s/47vqp?file=/src/App.tsx works.
 /*
@@ -40,17 +64,17 @@ class App extends Component {
     };
   }
 */
-  export default function App(){
-    const [state, dispatch] = useReducer(reducer, initialState);
-    //const [errState, setErrState] = useState({ error: false })
-    const exampleSpells = [
-      {
-        'code': `var hello = "hello World";
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  //const [errState, setErrState] = useState({ error: false })
+  const exampleSpells = [
+    {
+      code: `var hello = "hello World";
         console.log(hello);`,
-        'language': 'javascript'
-      },
-      {
-        'code': `  componentWillMount() {
+      language: "javascript",
+    },
+    {
+      code: `  componentWillMount() {
           client.onopen = () => {
             console.log('WebSocket Client Connected');
           };
@@ -58,20 +82,20 @@ class App extends Component {
             console.log(message);
           };
         }`,
-        'language': 'javascript'
-      },
-      {
-        'code': `const grimoire = Object.keys(state.spells).map(key=>{
+      language: "javascript",
+    },
+    {
+      code: `const grimoire = Object.keys(state.spells).map(key=>{
           return {
             key: key,
             gesture:state.spells[key]?.gesture, 
             words:state.spells[key]?.words
           };
         });`,
-        'language': 'javascript'
-      },
+      language: "javascript",
+    },
 
-      /*
+    /*
       {
         'code': `PROGRAM HELLO
     WRITE (*,100)
@@ -88,56 +112,136 @@ class App extends Component {
       'language': 'haskell'
     }
     */
-    ]
-    const grimoire = Object.keys(state.spells).map(key=>{
-      //let fn_arr = state.spells[key]?.spellname.split('.')
-      return {
-        key: key,
-        gesture:state.spells[key]?.gesture, 
-        //words:'.'+fn_arr[(fn_arr.length-1)]
-        words:state.spells[key]?.words
-      };
-    })
+  ];
+  const grimoire = Object.keys(state.spells).map((key) => {
+    //let fn_arr = state.spells[key]?.spellname.split('.')
+    return {
+      key: key,
+      gesture: state.spells[key]?.gesture,
+      //words:'.'+fn_arr[(fn_arr.length-1)]
+      words: state.spells[key]?.words,
+    };
+  });
 
-    // cdm
-    useEffect(() => {
-      // Set up socketio here
-      setupSocketEvents(dispatch);
-    }, []);
+  // cdm
+  useEffect(() => {
+    // Set up socketio here
+    setupSocketEvents(dispatch);
+  }, []);
 
-    //why am i getting max call stack exceeded errors from this?
-    // suspense doesnt really help in this case bc wont error out until it actually hits the max call stack 
-    // is this a new problem from my latest version of the @react-three/xr package?
-    //<Suspense fallback={<></>}></Suspense>
-    
+  //why am i getting max call stack exceeded errors from this?
+  // suspense doesnt really help in this case bc wont error out until it actually hits the max call stack
+  // is this a new problem from my latest version of the @react-three/xr package?
+  //<Suspense fallback={<></>}></Suspense>
+
   //render() {
 
-    return (
-      <DispatchContext.Provider value={dispatch}>
+  // ******************** Optomancy ********************
+
+  // ******************** Optomancy ********************
+  window.OptomancyNS.optomancy = new Optomancy(optomancyConfig);
+  window.OptomancyNS.optomancy.cast("x", {
+    field: "petalWidth",
+    type: "continuous",
+  });
+  window.OptomancyNS.optomancy.cast("y", {
+    field: "petalLength",
+    type: "continuous",
+  });
+  window.OptomancyNS.optomancy.cast("z", {
+    field: "sepalWidth",
+    type: "continuous",
+  });
+  window.OptomancyNS.optomancy.cast("color", {
+    field: "species",
+    type: "categorical",
+  });
+  // ***************************************************
+
+  const { data } = window.OptomancyNS.optomancy.config;
+  const { scales } = window.OptomancyNS.optomancy.propsExport;
+  // ***************************************************
+
+  return (
+    <DispatchContext.Provider value={dispatch}>
       <div>
-      {isHL ? 
+        {/* {isHL ? ( */}
         <VRCanvas>
-          <Hands /> 
+          <Hands />
           <SpellPages spells={exampleSpells} />
-          <MageHand grimoire={[...primitives, ...grimoire]} /> 
+          <MageHand grimoire={[...primitives, ...grimoire]} />
           <OrbitControls />
           <ambientLight />
           <pointLight position={[1, 1, 1]} />
-          <color args={['black']} attach="background" />
-        </VRCanvas>
-        : <Dictaphone />
-      }
-        {
-          grimoire.map((spell, index) => {
-              console.log(spell);
-              return (
-                <Interpreter key={spell.key} gesture={spell.gesture} words={spell.words}/>
-                )})
-        }
+          <color args={["black"]} attach="background" />
 
+          {/* VISUALIZATION */}
+          {/* <mesh position={[0, 0, 3]}> */}
+          <mesh position={[-1, 1, -1]}>
+            <>
+              {data.map((el: any, i: number) => {
+                return (
+                  <mesh
+                    key={`geom${i}`}
+                    position={[
+                      scales.x(el.petalWidth),
+                      scales.y(el.petalLength),
+                      scales.z(el.sepalWidth),
+                    ]}
+                  >
+                    <sphereGeometry attach="geometry" args={[0.01, 8, 8]} />
+                    <meshStandardMaterial
+                      attach="material"
+                      color={new THREE.Color(scales.color(el.species))}
+                      roughness={0.1}
+                      metalness={0.1}
+                    />
+                  </mesh>
+                );
+              })}
+              {/* Z */}
+              <mesh
+                position={[0, 0, 0.25]}
+                rotation={[THREE.MathUtils.degToRad(90), 0, 0]}
+              >
+                <cylinderGeometry attach="geometry" args={[0.01, 0.01, 0.5]} />
+                <meshBasicMaterial attach="material" color="white" />
+              </mesh>
+              {/* Y */}
+              <mesh
+                position={[0, 0.25, 0.5]}
+                rotation={[0, THREE.MathUtils.degToRad(90), 0]}
+              >
+                <cylinderGeometry attach="geometry" args={[0.01, 0.01, 0.5]} />
+                <meshBasicMaterial attach="material" color="white" />
+              </mesh>
+              {/* X */}
+              <mesh
+                position={[0.25, 0, 0.5]}
+                rotation={[0, 0, THREE.MathUtils.degToRad(90)]}
+              >
+                <cylinderGeometry attach="geometry" args={[0.01, 0.01, 0.5]} />
+                <meshBasicMaterial attach="material" color="white" />
+              </mesh>
+            </>
+          </mesh>
+        </VRCanvas>
+        {/* ) : ( */}
+        {/* <Dictaphone /> */}
+        {/* )} */}
+        {grimoire.map((spell, index) => {
+          console.log(spell);
+          return (
+            <Interpreter
+              key={spell.key}
+              gesture={spell.gesture}
+              words={spell.words}
+            />
+          );
+        })}
       </div>
-      </DispatchContext.Provider>
-    );
+    </DispatchContext.Provider>
+  );
   //}
 }
 
